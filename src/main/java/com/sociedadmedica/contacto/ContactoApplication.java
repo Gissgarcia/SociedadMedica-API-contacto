afc099926adb07581d3ce77db2dde411cc41c9e8
Gissgarcia <giss.garcia@duocuc.cl>
2025-11-30 00:49:59 -0300

    modificaciones

---
+++ src/main/java/com/sociedadmedica/contacto/ContactoApplication.java
@@ -0,0 +1,13 @@
+package com.sociedadmedica.contacto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContactoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContactoApplication.class, args);
+	}
+
+}
